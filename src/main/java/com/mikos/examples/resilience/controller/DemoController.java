1361c6dfbe08c54f8f981b8cbdaec992471de44f
Andre Micocci <andre.micocci@portoseguro.com.br>
2019-10-04 16:41:35 -0300

    Add code for Resilience4j

--- src/main/java/com/mikos/examples/resilience/controller/DemoController.java
+++ src/main/java/com/mikos/examples/resilience/controller/DemoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mikos.examples.resilience.model.Status;
 import com.mikos.examples.resilience.service.IntermittentService;
+import com.mikos.examples.resilience.service.Resilience4jService;
 import com.mikos.examples.resilience.service.WebFluxService;
 
 import reactor.core.publisher.Mono;
@@ -15,19 +16,28 @@ public class DemoController {
 
 	@Autowired
 	private final WebFluxService webFluxService;
+
+	@Autowired
+	private final Resilience4jService resilience4jService;
 	
 	@Autowired
 	private final IntermittentService intermittentService;
 	
 
-	public DemoController(WebFluxService webFluxService, IntermittentService intermittentService) {
+	public DemoController(WebFluxService webFluxService, Resilience4jService resilience4jService, IntermittentService intermittentService) {
 		this.webFluxService = webFluxService;
+		this.resilience4jService = resilience4jService;
 		this.intermittentService = intermittentService;
 	}
 
 	@GetMapping("/get")
 	public Mono<Status> get() {
 		return webFluxService.get();
+	}
+	
+	@GetMapping("/resilience4j")
+	public Mono<Status> getResilience4j() {
+		return resilience4jService.get();
 	}
 	
 	@GetMapping("/intermittent")
---
+++ src/main/java/com/mikos/examples/resilience/service/Resilience4jService.java
@@ -0,0 +1,63 @@
+package com.mikos.examples.resilience.service;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.mikos.examples.resilience.model.Status;
+
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import reactor.core.publisher.Mono;
+
+@Service
+public class Resilience4jService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Resilience4jService.class);
+
+	private WebClient webClient;
+	
+	@Autowired
+	private WebClient.Builder builderInjected;
+	
+	@Value("${api.intermittent.endpoint}")
+	private String endpoint;
+	
+	@PostConstruct
+	private void init() {
+		webClient = builderInjected
+				.baseUrl(endpoint)
+				.build();
+	}	
+	
+	@CircuitBreaker(name = "backendA", fallbackMethod = "fallback")
+	@RateLimiter(name = "backendA")
+	@Bulkhead(name = "backendA")
+	@Retry(name = "backendA", fallbackMethod = "fallback")
+	public Mono<Status> get() {
+		LOGGER.debug("Resilience4jService.get......");
+		
+		return webClient
+				.get()
+				.uri("/intermittent")
+				.retrieve()
+				.bodyToMono(Status.class);
+//				.doOnError(err -> LOGGER.error(err.getMessage()))
+//				.retryBackoff(retryMax, Duration.ofMillis(retryInterval))
+
+	}
+
+	private Mono<Status> fallback(IllegalArgumentException e) {
+		return Mono.just(new Status("DOWN","Fallback"));
+	}
+
+	private Mono<Status> fallback(RuntimeException e) {
+		return Mono.just(new Status("DOWN","Fallback"));
+	}
+}
